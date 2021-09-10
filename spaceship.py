59ff420a4be7668c09822cbba22ce62d806e5ab9
Challix <55164668+Challix@users.noreply.github.com>
2021-09-10 15:41:44 -0400

    Add files via upload

---
+++ spaceship.py
@@ -0,0 +1,20 @@
+#########################
+# Spaceship Starter Code
+#
+# Author: Your_name_here
+#########################
+
+print("Welcome traveler!")
+
+name = input("What is your name? ")
+
+print("Hello {}!".format(name))
+
+print("How many companions do you have?")
+companions = 0
+
+# Add your input code here
+
+print("Where do you want to go?")
+
+# Add your code here
